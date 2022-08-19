0d7e1790005a861ae9625241f9498ae350d26d4a
mercy-yugi <mercyyugi@gmail.com>
2022-08-19 17:21:35 +0300

    first commit

---
+++ Digitalwallet/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from .models import Account, Card, Customer, Loan, Notifications, Receipt, Reward, ThirdParty, Transaction, Wallet
+
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('first_name','last_name','age','email',)
+    search_fields = ('first_name', 'last_name',)
+admin.site.register(Customer, CustomerAdmin)
+class WalletAdmin(admin.ModelAdmin):
+    list_display = ('user_id','amount','balance','status','date')
+    search_fields = ('user_id','amount','balance',)
+admin.site.register(Wallet)
+
+admin.site.register(Account)
+admin.site.register(Transaction)
+admin.site.register(Card)
+admin.site.register(ThirdParty)
+admin.site.register(Notifications)
+admin.site.register(Receipt)
+admin.site.register(Loan)
+admin.site.register(Reward)
